3e2c924608d6cc81a249fcf8662a8e93a3699116
ShilpaMachinapally <machinapallyshilpa@gmail.com>
2026-05-02 09:26:34 +0530

    Fixed DB config for local + deployment

--- database/connection.py
+++ database/connection.py
@@ -10,14 +10,22 @@
 #     )
 
 
-
 import mysql.connector as SQLC
+import os
 
 def databaseConfig():
-    db_config = SQLC.connect(
-        host='shilpa1317.mysql.pythonanywhere-services.com',
-        user='shilpa1317',
-        password='YOUR_DB_PASSWORD',
-        database='shilpa1317$ecommerce1'
-    )
-    return db_config
+    # detect PythonAnywhere
+    if "pythonanywhere" in os.getcwd():
+        return SQLC.connect(
+            host='shilpa1317.mysql.pythonanywhere-services.com',
+            user='shilpa1317',
+            password='YOUR_ACTUAL_DB_PASSWORD',   # ⚠️ replace this
+            database='shilpa1317$ecommerce1'
+        )
+    else:
+        return SQLC.connect(
+            host='localhost',
+            user='root',
+            password='root',
+            database='ecommerce1'
+        )
